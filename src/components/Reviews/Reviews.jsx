0185fc68a8e5a44370506357b2ddd79b8441f905
Vladyslav-Markevych <vladen035@gmail.com>
2024-06-15 05:04:25 +0300

    add title

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -12,38 +12,43 @@ import "./style.css";
 
 export function Reviews() {
   return (
-    <ul>
-      <Swiper
-        modules={[Navigation, Pagination, Scrollbar, A11y]}
-        spaceBetween={50}
-        slidesPerView={3}
-        navigation
-        pagination={{ clickable: true }}
-        scrollbar={{ draggable: true }}
-      >
-        {descArray.map(({ text, id, name, job }) => {
-          return (
-            <SwiperSlide key={id}>
-              <li className='listItem'>
-                <img className='quote' src={quote} alt='quotes' />
-                {/* Нагуглил, пишут, что так не совсем правильно */}
-                <p
-                  className='descriptionText'
-                  dangerouslySetInnerHTML={{ __html: text }}
-                ></p>
-                <span className='initial'>
-                  {name
-                    .split(" ")
-                    .map((word) => word[0])
-                    .join("")}
-                </span>
-                <span className='nameItem'>{name}</span>
-                <p className='jobItem'>{job}</p>
-              </li>
-            </SwiperSlide>
-          );
-        })}
-      </Swiper>
-    </ul>
+    <>
+      <p className='swipe-title'>
+        How learners like you are achieving their goals
+      </p>
+      <ul>
+        <Swiper
+          modules={[Navigation, Pagination, Scrollbar, A11y]}
+          spaceBetween={50}
+          slidesPerView={3}
+          navigation
+          pagination={{ clickable: true }}
+          scrollbar={{ draggable: true }}
+        >
+          {descArray.map(({ text, id, name, job }) => {
+            return (
+              <SwiperSlide key={id}>
+                <li className='listItem'>
+                  <img className='quote' src={quote} alt='quotes' />
+                  {/* Нагуглил, пишут, что так не совсем правильно */}
+                  <p
+                    className='descriptionText'
+                    dangerouslySetInnerHTML={{ __html: text }}
+                  ></p>
+                  <span className='initial'>
+                    {name
+                      .split(" ")
+                      .map((word) => word[0])
+                      .join("")}
+                  </span>
+                  <span className='nameItem'>{name}</span>
+                  <p className='jobItem'>{job}</p>
+                </li>
+              </SwiperSlide>
+            );
+          })}
+        </Swiper>
+      </ul>
+    </>
   );
 }
